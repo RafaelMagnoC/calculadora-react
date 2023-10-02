448077e03d265f88581cd50a583415ad70ce922e
Rafael Magno <rafaelmagno125@yahoo.com.br>
2023-10-02 10:08:26 -0300

    logica que permite a calculadora funcionar

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -7,10 +7,9 @@ import "./Calculadora.css";
 const estadoInicialCalculadora = {
     valorAtualDoDisplay: "0",
     limparDisplay: false,
-    armezarTemporariamenteAOperacaoDeCalculo: null,
+    armezarOperacaoDeCalculo: null,
     valoresASeremCalculados: [0, 0],
     indiceDoValorQueEstaSendoDigitado: 0
-
 }
 
 
@@ -30,30 +29,58 @@ class Calculadora extends Component {
         this.setState({ ...estadoInicialCalculadora });
     }
 
-    enviarOperacao(operacao) {
-        console.log(operacao);
+    enviarOperacao(armezarOperacaoDeCalculo) {
+        if (this.state.indiceDoValorQueEstaSendoDigitado === 0) {
+            this.setState({
+                armezarOperacaoDeCalculo,
+                indiceDoValorQueEstaSendoDigitado: 1,
+                limparDisplay: true
+            });
+        }
+        else {
+            const resultado = armezarOperacaoDeCalculo === "=";
+            const operacaoRealizada = this.state.armezarOperacaoDeCalculo;
+
+            const valoresASeremCalculados = [...this.state.valoresASeremCalculados]
+            try {
+
+                valoresASeremCalculados[0] = eval(`${valoresASeremCalculados[0]} ${operacaoRealizada} ${valoresASeremCalculados[1]}`);
+            }
+            catch (e) {
+                valoresASeremCalculados[1] = 0;
+            }
+
+            this.setState({
+                valorAtualDoDisplay: valoresASeremCalculados[0],
+                armezarOperacaoDeCalculo: resultado ? null : armezarOperacaoDeCalculo,
+                indiceDoValorQueEstaSendoDigitado: resultado ? 0 : 1,
+                limparDisplay: !resultado,
+                valoresASeremCalculados
+            })
+        }
     }
 
     adicionarDigito(n) {
+
 
         if (n === "." && this.state.valorAtualDoDisplay.includes(".")) {
             return;
         }
 
-        const limparDisplaAgora = this.state.valorAtualDoDisplay === "0" || this.state.limparDisplay;
+        const limparDisplay = this.state.valorAtualDoDisplay === "0" || this.state.limparDisplay;
 
-        const valorAtual = limparDisplaAgora ? "" : this.state.valorAtualDoDisplay;
+        const valorAtual = limparDisplay ? "" : this.state.valorAtualDoDisplay;
 
-        const valorAcumulado = valorAtual + n;
+        const valorAtualDoDisplay = valorAtual + n;
 
-        this.setState({ valorAtualDoDisplay: valorAcumulado, limparDisplaAgora: false })
+        this.setState({ valorAtualDoDisplay, limparDisplay: false })
 
         if (n !== ".") {
             const indice = this.state.indiceDoValorQueEstaSendoDigitado;
-            const valorStringParaFloat = parseFloat(valorAcumulado);
-            const valor = [...this.state.valoresASeremCalculados];
-            valor[indice] = valorStringParaFloat;
-            this.setState({ valoresASeremCalculados: valor });
+            const valorStringParaFloat = parseFloat(valorAtualDoDisplay);
+            const valoresASeremCalculados = [...this.state.valoresASeremCalculados];
+            valoresASeremCalculados[indice] = valorStringParaFloat;
+            this.setState({ valoresASeremCalculados });
         }
 
     }
